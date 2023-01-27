901cf9c179fceeb78775bb30d380635a214584da
Rohit-huta <70508911+Rohit-huta@users.noreply.github.com>
2023-01-27 19:48:32 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+
+<?php include('partials/menu.php');?>
+        <!-- Main Content Section -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>DASHBOARD</h1>
+
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br>
+                    Categories
+                </div>
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br>
+                    Categories
+                </div>
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br>
+                    Categories
+                </div>
+                <div class="col-4 text-center">
+                    <h1>5</h1>
+                    <br>
+                    Categories
+                </div>  
+            </div>
+
+            <div class="clear-fix"></div>
+        </div>
+        
+        <!-- Footer -->
+
+        <?php include('partials/footer.php')?>
